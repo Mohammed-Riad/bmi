6fe133dd02e3786244e9a4828d38629e30f33af3
Mohammed-Riad <hamodehriad77@gmail.com>
2022-11-02 00:23:19 +0200

    add

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import './App.css';
+import Headr  from './Headr';
+import Footer  from './Footer';
+import Formcalc  from './Formcalc';
+function App() {
+  return (
+    <div>
+    <Headr />
+    <Formcalc />
+    <Footer />
+    </div>
+
+  );
+}
+
+export default App;
---
+++ src/Footer.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+export default class footer extends Component {
+  render() {
+    
+    return (
+      <div>
+
+
+<footer class="page-footer font-small blue" style={{background:"blue" , }}>
+
+
+  <div class="footer-copyright text-center py-3">© 2020 Copyright:
+    <a href="/"> MDBootstrap.com</a>
+  </div>
+  
+
+</footer>
+
+      </div>
+    );
+  }
+}
